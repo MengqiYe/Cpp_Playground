a6437f1d5b37eb5fb1287babcd55f82a8a91b56a
mqye <mengqi.ye@anlogic.com>
2022-02-16 19:52:13 +0800

    Add std.transform omp example.

---
+++ std.transform/omp.cpp
@@ -0,0 +1,44 @@
+//
+// Created by elsey on 2022/2/16.
+//
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+class Dat {
+public:
+    Dat();
+    int x;
+    int y;
+    int z;
+};
+
+void write_to_cout(std::vector<Dat> &in) {
+    for (int i = 0; i < in.size(); ++i) {
+        std::cout << in[i].x << ":" << in[i].y << ":" << in[i].z << ", ";
+    }
+}
+
+int main() {
+    std::vector<Dat> a = {Dat(), Dat(), Dat(), Dat(), Dat()};
+    std::vector<Dat> b = {Dat(), Dat(), Dat(), Dat(), Dat()};
+    std::vector<Dat> c;
+
+    // Write initial states
+    write_to_cout(a);
+    std::cout << std::endl;
+    write_to_cout(b);
+    std::cout << std::endl;
+
+    // Test algorithm
+    std::transform(
+            a.begin(), a.end(), b.begin(), std::back_inserter(c),
+            [](Dat a, Dat b) { return Dat(); }
+    );
+
+    write_to_cout(c);
+    std::cout << std::endl;
+
+    return 0;
+}
